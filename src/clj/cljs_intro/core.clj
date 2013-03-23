c8fda036c29a8bb29423086ad212b32efa80ba27
oliyh <git@oliy.co.uk>
2013-03-23 09:35:18 -0700

    modifying to use compojure and lib-noir instead of noir

--- src/clj/cljs_intro/core.clj
+++ src/clj/cljs_intro/core.clj
@@ -1,16 +1,23 @@
 (ns cljs-intro.core
-	(:use noir.core)
-	(:require [noir.server :as server]
-					  [noir.response :as response]
-						[cljs-intro.db :as db]))
+  (:use [compojure.core]
+        [hiccup.core]
+        [hiccup.form]
+        [ring.middleware.params :only [wrap-params]])
+  (:require [ring.adapter.jetty :as jetty]
+            [compojure.route :as route]
+            [hiccup.core :as hiccup]
+            [cljs-intro.db :as db]
+            [noir.response :as response]))
 
-; responds to HTTP GET calls for the URL
-; Passes the player's last name to the DB 
-; access code to get the player's stats.
-; Returns results in JSON					
-(defpage "/player/:lastname" {:keys [lastname]}
+(defn player [lastname]
   (response/json (db/get-player lastname)))
 
+(defroutes app-routes 
+;;  (GET "/" [] (message "home"))
+  (GET "/player/:lastname" [lastname] (player lastname))
+;;  (POST "/" [id message] (display-message id message))
+  (route/resources "/")
+  (route/not-found "Not Found"))
+  
 (defn -main []
-	(server/start 8888))
- 
+  (jetty/run-jetty (wrap-params app-routes) {:port 8888}))
